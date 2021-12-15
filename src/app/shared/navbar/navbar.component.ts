06a13398e0cfbe2569e2a3a4dee37d10e9eef054
Gulce Sirvanci <gulcesirvanci@gmail.com>
2021-12-15 21:33:15 +0300

    Added Config Page

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -43,7 +43,7 @@ export class NavbarComponent implements OnInit{
               return this.listTitles[item].title;
           }
       }
-      return 'Dashboard';
+      return '';
     }
     sidebarToggle() {
         if (this.sidebarVisible === false) {
